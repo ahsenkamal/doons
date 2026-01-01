a89f4bde5516eb4b19bd642ebfaf57485e0b3bb4
Ahsen Kamal <itsahsenkamal@gmail.com>
2026-01-01 13:36:21 +0530

    basic server
    Signed-off-by: Ahsen Kamal <itsahsenkamal@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,17 @@
-use std::fs::File;
-use std::io::{Read, Result};
+use std::io::Result;
 use std::net::UdpSocket;
 
 mod protocol;
-use protocol::{BytePacketBuffer, DnsPacket, DnsQuestion, QueryType};
+mod server;
+use server::utils::handle_query;
 
 fn main() -> Result<()> {
-    let qname = "yahoo.com";
-    let qtype = QueryType::MX;
-    let server = ("8.8.8.8", 53);
+    let socket = UdpSocket::bind(("0.0.0.0", 2053))?;
 
-    let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
-
-    let mut packet = DnsPacket::new();
-
-    packet.header.id = 6666;
-    packet.header.questions = 1;
-    packet.header.recursion_desired = true;
-    packet
-        .questions
-        .push(DnsQuestion::new(qname.to_string(), qtype));
-
-    let mut req_buffer = BytePacketBuffer::new();
-    packet.write(&mut req_buffer)?;
-    socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
-
-    let mut res_buffer = BytePacketBuffer::new();
-    socket.recv_from(&mut res_buffer.buf)?;
-
-    let res_packet = DnsPacket::from_buffer(&mut res_buffer)?;
-    println!("{:#?}", res_packet.header);
-
-    for q in res_packet.questions {
-        println!("{:#?}", q);
+    loop {
+        match handle_query(&socket) {
+            Ok(_) => {}
+            Err(e) => eprintln!("An error occurred: {}", e),
+        }
     }
-    for rec in res_packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.resources {
-        println!("{:#?}", rec);
-    }
-
-    Ok(())
 }
